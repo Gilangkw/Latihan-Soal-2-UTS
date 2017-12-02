d0fc4ca835988178791adf70e38566e941a6a632
Titanfrs <32485223+Titanfrs@users.noreply.github.com>
2017-12-02 21:34:57 +0700

    Add files via upload

---
+++ UTSsoal2-Titan/src/Model/UTS2.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author 99
+ */
+public class UTS2 {
+    private String nama;
+    private String noKtp;
+    private String cabang;
+    private String handuk;
+    private String paket;
+    private String jenKel;
+    private int biaya;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoKtp() {
+        return noKtp;
+    }
+
+    public void setNoKtp(String noKtp) {
+        this.noKtp = noKtp;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getHanduk() {
+        return handuk;
+    }
+
+    public void setHanduk(String handuk) {
+        this.handuk = handuk;
+    }
+
+    public String getPaket() {
+        return paket;
+    }
+
+    public void setPaket(String paket) {
+        this.paket = paket;
+    }
+
+    public String getJenKel() {
+        return jenKel;
+    }
+
+    public void setJenKel(String jenKel) {
+        this.jenKel = jenKel;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ 
+    
+    }
+    
+}
